d9b4bf89a346fa37a0808562908eb7f38a855f9a
MarcosMoura <marcos_moura@live.com>
2019-07-06 07:09:51 -0300

    Exibindo Erro e Loading

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import {
-  View, Text, TextInput, TouchableOpacity, StatusBar,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  ActivityIndicator,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../../services/api';
@@ -11,6 +16,8 @@ class Welcome extends Component {
   // 1. Armazena o texto do input (username) dentro do estado do component.
   state = {
     username: '',
+    loading: false,
+    error: false,
   };
 
   componentDidMount() {}
@@ -31,25 +38,30 @@ class Welcome extends Component {
     const { username } = this.state; // vou buscar o username dentro do estado
     const { navigation } = this.props; // desestrutura para fazer uso da propriedade navigate.
 
+    this.setState({ loading: true }); // Se der sucesso ele vai pra próxima página
+
     try {
       await this.checkUserExists(username);
       await this.saveUser(username); // Salva o usuário no storage da aplicação
       // console.tron.log(this.props.navigation);
       navigation.navigate('Repositories'); // utilizo o navigate passando o nome da prox tela.
     } catch (err) {
+      this.setState({ loading: false, error: true });
       console.tron.log('Usuário Inexistente');
     }
   };
 
   render() {
     // 2. Pega o username do estado
-    const { username } = this.state;
+    const { username, loading, error } = this.state;
 
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Bem-vindo</Text>
         <Text style={styles.text}>Para continuar precisamos que você informe seu usuário</Text>
+
+        {error && <Text style={styles.error}>Usuário Inexistente</Text>}
 
         <View style={styles.form}>
           <TextInput
@@ -63,7 +75,11 @@ class Welcome extends Component {
           />
 
           <TouchableOpacity style={styles.button} onPress={this.SignIn}>
-            <Text style={styles.buttonText}>Prosseguir</Text>
+            {loading ? ( // Se eu tiver com o loading irei mostrar um activityIndicator se não coloco o buttonText normal, isso serve para mostrar o loading no botão.
+              <ActivityIndicator size="small" color="#FFF" />
+            ) : (
+              <Text style={styles.buttonText}>Prosseguir</Text>
+            )}
           </TouchableOpacity>
         </View>
       </View>

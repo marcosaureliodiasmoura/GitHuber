25d977d7db5f3042c9b75c37b5b3d957f923103c
MarcosMoura <marcos_moura@live.com>
2019-07-08 07:29:37 -0300

    Pagina de Organizações

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-import { View, Text } from 'react-native';
+import {
+  View, AsyncStorage, FlatList, ActivityIndicator,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import OrganizationItem from './OrganizationItem';
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
+import styles from './styles';
 
 // Segunda forma de fazer para evitar erro de PropTypes
 // Criamos em formato de função para pegarmos a tintColor para repassarmos pro nosso icone.
@@ -21,8 +19,64 @@ TabIcon.propTypes = {
   tintColor: PropTypes.string.isRequired,
 };
 
-Organizations.navigationOptions = {
-  tabBarIcon: TabIcon,
-};
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-export default Organizations;
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false, // anotação a informação de quando esta executando o refresh ou nao.
+  };
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true }); // Essa função vai ser chamada toda vez que usuário arrastar para baixo, diferente do load
+    const username = await AsyncStorage.getItem('@Githuber:username'); // Vou usar este username para buscar os repositorios da API.
+
+    const { data } = await api.get(`/users/${username}/orgs`); // Busco todos os repositorios referente ao username que foi digitado.
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  // Pra pegar o conteúdo de cada repositorio do Item de Flatlist e
+  // Repassar para o componente RepositoryItem que ta esperando uma
+  // Propriedade repository, vou passar repository dentro e
+  // Pra pegar o item do Flatlist terei que passar uma desestruturação do
+  // Objeto que eu recebo e então passar o item.
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />;
+
+  // renderList = () => <Text>Lista</Text>;
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data} // Qual o array onde está os meus dados? nosso data do state
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+        onRefresh={this.loadRepositories} // onRefresh faz com que apareça uma flecha no ato de carregar novos itens na tela ao arrastar a lista pra baixo.
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+        {/* {this.state.data.map(repo => (
+          <Text>{repo.name}</Text>
+        ))} */}
+      </View>
+    );
+  }
+}
